9567056b5a0d2aac6f3e45a80c540f7307519941
Salatiel dos Reis <salatieldrg@gmail.com>
2022-07-15 14:04:10 -0300

    Parseando valores para listas

--- src/consumidorApi/ConsumidorImdb.java
+++ src/consumidorApi/ConsumidorImdb.java
@@ -7,6 +7,11 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.time.Duration;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ConsumidorImdb {
 	public static void main(String[] args) throws IOException, InterruptedException {
@@ -27,5 +32,47 @@ public class ConsumidorImdb {
 		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 		
 		System.out.println(response.body());
+		
+		String json = response.body();
+		
+		String[] moviesArray = parseJsonMovies(json);
+		
+		List<String> titles = parseTitles(moviesArray);
+		titles.forEach(System.out::println);
+
+		List<String> urlImages = parseUrlImages(moviesArray);
+		urlImages.forEach(System.out::println);
+		
+	}
+	
+	private static String[] parseJsonMovies(String json) {
+		Matcher matcher = Pattern.compile(".*\\[(.*)\\].*").matcher(json);
+
+		if (!matcher.matches()) {
+			throw new IllegalArgumentException("no match in " + json);
+		}
+
+		String[] moviesArray = matcher.group(1).split("\\},\\{");
+		moviesArray[0] = moviesArray[0].substring(1);
+		int last = moviesArray.length - 1;
+		String lastString = moviesArray[last];
+		moviesArray[last] = lastString.substring(0, lastString.length() - 1);
+		return moviesArray;
+	}
+	
+	private static List<String> parseTitles(String[] moviesArray) {
+		return parseAttribute(moviesArray, 3);
+	}
+	
+	private static List<String> parseUrlImages(String[] moviesArray) {
+		return parseAttribute(moviesArray, 5);
+	}
+	
+	private static List<String> parseAttribute(String[] moviesArray, int pos) {
+		return Stream.of(moviesArray)
+			.map(e -> e.split("\",\"")[pos]) 
+			.map(e -> e.split(":\"")[1]) 
+			.map(e -> e.replaceAll("\"", ""))
+			.collect(Collectors.toList());
 	}
 }
